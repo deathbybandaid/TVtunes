89a70913e86dc69936d66ee793bde688dd869a24
deathbybandaid <github@deathbybandaid.net>
2021-06-23 09:16:31 -0400

    test

--- TVtunes/plexinterface/__init__.py
+++ TVtunes/plexinterface/__init__.py
@@ -16,11 +16,7 @@ class PlexInterface():
 
         first_show_lib = self.list_tv_libraries[0]
         first_show = self.list_library_shows(first_show_lib)[0]
-        # print(dir(first_show))
-        """
-        ['METADATA_TYPE', 'TAG', 'TYPE', '_INCLUDES', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_autoReload', '_buildDetailsKey', '_buildItem', '_buildItemOrNone', '_castAttrValue', '_checkAttrs', '_clean', '_data', '_defaultSyncTitle', '_details_key', '_edit_tags', '_getAttrOperator', '_getAttrValue', '_initpath', '_isChildOf', '_loadData', '_manuallyLoadXML', '_parent', '_reload', '_server', 'actors', 'addCollection', 'addGenre', 'addLabel', 'addedAt', 'analyze', 'art', 'artBlurHash', 'artUrl', 'arts', 'audienceRating', 'audienceRatingImage', 'augmentation', 'autoDeletionItemPolicyUnwatchedLibrary', 'autoDeletionItemPolicyWatchedLibrary', 'banner', 'bannerUrl', 'banners', 'childCount', 'collections', 'contentRating', 'defaultAdvanced', 'delete', 'download', 'duration', 'edit', 'editAdvanced', 'episode', 'episodeSort', 'episodes', 'fetchItem', 'fetchItems', 'fields', 'findItems', 'firstAttr', 'fixMatch', 'flattenSeasons', 'genres', 'get', 'guid', 'guids', 'history', 'hubs', 'index', 'isFullObject', 'isPartialObject', 'isWatched', 'key', 'labels', 'languageOverride', 'lastRatedAt', 'lastViewedAt', 'leafCount', 'librarySectionID', 'librarySectionKey', 'librarySectionTitle', 'listAttrs', 'listType', 'locations', 'markUnwatched', 'markWatched', 'matches', 'merge', 'network', 'onDeck', 'optimize', 'originalTitle', 'originallyAvailableAt', 'posterUrl', 'posters', 'preference', 'preferences', 'rate', 'rating', 'ratingKey', 'refresh', 'reload', 'removeCollection', 'removeGenre', 'removeLabel', 'removeSubtitles', 'roles', 'season', 'seasons', 'section', 'setArt', 'setBanner', 'setPoster', 'showOrdering', 'similar', 'split', 'studio', 'subtitleStreams', 'summary', 'sync', 'tagline', 'theme', 'thumb', 'thumbBlurHash', 'thumbUrl', 'title', 'titleSort', 'type', 'unmatch', 'unwatched', 'updatedAt', 'uploadArt', 'uploadBanner', 'uploadPoster', 'uploadSubtitles', 'url', 'useOriginalTitle', 'userRating', 'viewCount', 'viewedLeafCount', 'watched', 'year']
-        """
-        print(first_show.locations)
+        print(self.show_location(first_show_lib, first_show))
 
     def connect(self):
         self.tvtunes.logger.info("Attempting Connection to Plex Media Server at %s:%s" % (self.address, self.port))
@@ -48,35 +44,44 @@ class PlexInterface():
     def total_libraries(self):
         if not self.plexserver:
             return 0
-        else:
-            return len(self.plexserver.library.sections())
+
+        return len(self.plexserver.library.sections())
 
     @property
     def total_tv_libraries(self):
         if not self.plexserver:
             return 0
-        else:
-            return len([x for x in self.plexserver.library.sections() if x.type == "show"])
+
+        return len([x for x in self.plexserver.library.sections() if x.type == "show"])
 
     @property
     def list_tv_libraries(self):
         if not self.plexserver:
             return []
-        else:
-            return [x.title for x in self.plexserver.library.sections() if x.type == "show"]
+
+        return [x.title for x in self.plexserver.library.sections() if x.type == "show"]
 
     @property
     def total_tv_shows(self):
         if not self.plexserver:
             return 0
-        else:
-            total_shows = 0
-            for x in self.list_tv_libraries:
-                total_shows += len(self.list_library_shows(x))
-            return total_shows
+
+        total_shows = 0
+        for x in self.list_tv_libraries:
+            total_shows += len(self.list_library_shows(x))
+        return total_shows
 
     def list_library_shows(self, library):
+        if not self.plexserver:
+            return []
         return self.plexserver.library.section(library).all()
+
+    def show_location(self, library, show):
+        if not self.plexserver:
+            return None
+
+        show_item = self.plexserver.library.section(library).get(show)
+        return show_item.locations
 
     @property
     def baseurl(self):
