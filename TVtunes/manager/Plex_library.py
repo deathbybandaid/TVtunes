d7cbca345a0cb9ab4758b216a8dbb3b18d7732f8
deathbybandaid <github@deathbybandaid.net>
2021-06-23 09:29:06 -0400

    test

---
+++ TVtunes/manager/Plex_library.py
@@ -0,0 +1,9 @@
+
+
+class Plex_library():
+    """
+    Methods for Cataloging a Plex Library.
+    """
+
+    def __init__(self, tvtunes, plexinterface, library_id):
+        pass
--- TVtunes/manager/__init__.py
+++ TVtunes/manager/__init__.py
@@ -1,5 +1,8 @@
 
 
+from .plex_library import Plex_library
+
+
 class TVtunes_Manager():
     """
     Methods for Managing Plex Libraries, Shows, and their Theme Music.
@@ -8,3 +11,21 @@ class TVtunes_Manager():
     def __init__(self, tvtunes, plexinterface):
         self.tvtunes = tvtunes
         self.plexinterface = plexinterface
+
+        self.get_db_libraries()
+
+    def get_db_libraries(self):
+        """
+        Retrieve existing libraries from database.
+        """
+
+        self.tvtunes.logger.info("Checking for Library information stored in the database.")
+        library_ids = self.tvtunes.db.get_tvtunes_value("libraries", "list") or []
+
+        if len(library_ids):
+            self.tvtunes.logger.info("Found %s existing libraries in the database." % str(len(library_ids)))
+
+        for library_id in library_ids:
+            library_ids_obj = Plex_library(self.tvtunes, self.plexinterface, library_id=library_id)
+            library_id = library_ids_obj.dict["id"]
+            self.list[library_id] = library_ids_obj
