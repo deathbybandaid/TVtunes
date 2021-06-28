0faa87ad9e670162eb9562d53c36b28b2cc85f50
deathbybandaid <github@deathbybandaid.net>
2021-06-28 13:56:43 -0400

    test

--- TVtunes_web/pages/tvshows_html.py
+++ TVtunes_web/pages/tvshows_html.py
@@ -23,6 +23,7 @@ class TVShows_HTML():
             if show_obj:
                 show_dict = show_obj.dict.copy()
                 show_dict["theme_file"] = show_obj.theme_file
+                show_dict["theme_link"] = show_obj.plexcom_theme_url
                 show_library = show_obj.library
 
                 if show_library not in list(shows_info.keys()):
