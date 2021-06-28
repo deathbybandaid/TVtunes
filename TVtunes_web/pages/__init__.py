9ae7da437e808ad63f1f741ed5461d0be9719a40
deathbybandaid <github@deathbybandaid.net>
2021-06-28 12:35:12 -0400

    test

--- TVtunes_web/pages/__init__.py
+++ TVtunes_web/pages/__init__.py
@@ -4,6 +4,7 @@ from .index_html import Index_HTML
 from .versions_html import Versions_HTML
 from .diagnostics_html import Diagnostics_HTML
 from .settings_html import Settings_HTML
+from .tvshows_html import TVShows_HTML
 
 
 class TVtunes_Pages():
@@ -15,3 +16,5 @@ class TVtunes_Pages():
         self.versions_html = Versions_HTML(tvtunes)
         self.diagnostics_html = Diagnostics_HTML(tvtunes)
         self.settings_html = Settings_HTML(tvtunes)
+
+        self.tvshows_html = TVShows_HTML(tvtunes)
---
+++ TVtunes_web/pages/tvshows_html.py
@@ -0,0 +1,38 @@
+from flask import request, render_template, session
+
+
+class TVShows_HTML():
+    endpoints = ["/tvshows", "/tvshows.html"]
+    endpoint_name = "page_tvshows_html"
+    endpoint_access_level = 1
+    endpoint_category = "tool_pages"
+    pretty_name = "Versions"
+
+    def __init__(self, tvtunes):
+        self.tvtunes = tvtunes
+
+    def __call__(self, *args):
+        return self.get(*args)
+
+    def get(self, *args):
+
+        shows_info = {}
+
+        for show_id in [x["id"] for x in self.tvtunes.tvshows.get_shows()]:
+            show_obj = self.tvtunes.tvshows.list[show_id]
+            if show_obj:
+                show_dict = show_obj.dict.copy()
+                show_library = show_obj.library
+
+                if show_library not in list(shows_info.keys()):
+                    shows_info[show_library] = {}
+
+                shows_info[show_library][show_obj.title] = show_dict
+
+        libraries = list(shows_info.keys())
+
+        library = request.args.get('library', default=libraries[0], type=str)
+        if library not in libraries:
+            library = libraries[0]
+
+        return render_template('tvshows.html', request=request, session=session, tvtunes=self.tvtunes, shows_info=shows_info, libraries=libraries, library=library)
