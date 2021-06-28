b5a770c4ad39457198afe99b70e88bcfc51a26d7
deathbybandaid <github@deathbybandaid.net>
2021-06-28 11:22:42 -0400

    test

--- TVtunes/plexinterface/__init__.py
+++ TVtunes/plexinterface/__init__.py
@@ -98,11 +98,7 @@ class PlexInterface():
         if not self.plexserver:
             return None
 
-        try:
-            show_item = self.plexserver.library.section(library).get(show)
-        except TypeError:
-            return None
-
+        show_item = self.plexserver.library.section(library).get(show)
         guids = [str(item_guid.id).split("tvdb://")[-1] for item_guid in show_item.guids if str(item_guid.id).startswith("tvdb")]
         if not len(guids):
             return None
